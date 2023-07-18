d7e037d8601e0cd7febdc9e3a1510699c077fc56
Telman <tiisla10@mts.ru>
2023-07-18 11:14:34 +0300

    Chapter 18 "Types of Values" 	- added support for 'NIL', 'boolean' and 'number' data types. 	- implemented type check macros.

--- src/constant_pool.h
+++ src/constant_pool.h
@@ -3,7 +3,42 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER
+} ValueType;
+
+/** Memory layout prepresentation is as follows:
+ * ___________________type
+ * |	  |
+ * |	  |___________padding
+ * |	  ||	  |
+ * |	  ||	  |_________________as
+ * |	  ||	  ||______________|
+ * |	  ||	  ||__bool		  |
+ * |	  ||	  || _____double__|
+ * |	  ||	  || |			  |
+ * [][][][][][][][][][][][][][][][]
+ */
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
+
+#define IS_BOOL(value)		((value).type == VAL_BOOL)
+#define IS_NIL(value)		((value).type == VAL_NIL)
+#define IS_NUMBER(value)	((value).type == VAL_NUMBER)
+
+#define BOOL_PACK(value)	((Value){VAL_BOOL,	 {.boolean = value}})
+#define NIL_PACK			((Value){VAL_NIL,	 {.number = 0}})
+#define NUMBER_PACK(value)	((Value){VAL_NUMBER, {.number = value}})
+
+#define BOOL_UNPACK(value)		((value).as.boolean)
+#define NUMBER_UNPACK(value)	((value).as.number)
 
 typedef struct {
 	uint32_t capacity;
