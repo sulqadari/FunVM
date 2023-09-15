6f8b1286b98273a703cf22dede19b5da5aceabd3
Telman <tiisla10@mts.ru>
2023-09-15 14:40:34 +0300

    discarded FreeBSD-specific auto-included header files.

--- src/bytecode.c
+++ src/bytecode.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 #include <stdlib.h>
-//#include <sys/endian.h>
 #include "bytecode.h"
 #include "memory.h"
 
--- src/compiler.c
+++ src/compiler.c
@@ -5,8 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/_stdint.h>
-#include <sys/endian.h>
 
 #include "bytecode.h"
 #include "common.h"
