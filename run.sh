a3abcb9b4013b9ca27cbe26a9920d2b3fc273e9b
Telman Islamov <sulqadari@gmail.com>
2023-10-08 23:13:16 +0300

    revision chapter 24 "Calls and Functions"  - unit-test scripts removed from building script;  - run.sh script iterates through the whole files in test_scripts    folder;
    TODO:
     - proceed to clause 24.4 "Function Declarations"

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "************ RUNNING FUNCTIONAL TESTS ****************"
+echo " "
+
+
+for currScript in ./test_scripts/functional/*.fn
+do
+	echo "*** $currScript ***"
+	./build/FunVM $currScript
+	if [ "$?" -ne 0 ]; then
+		echo "Tests execution have been interrupted."
+		exit 1
+	fi
+	echo "PASSED"
+	echo " "
+done
+
+echo " "
+echo "*** FUNCTIONAL TESTS HAVE BEEN PASSED SUCCESSFULLY ***"
+echo " "
