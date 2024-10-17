1ba80f82d441a13beea342d365215285b0c74346
Telman Islamov <t.islamov@aladdin.ru>
2024-10-17 17:56:02 +0300

    chapter 17: completed.
    TODO: revision chapters 14-17

---
+++ common/common.h
@@ -0,0 +1,10 @@
+#ifndef FUNVM_COMMON_H
+#define FUNVM_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* FUNVM_COMMON_H */
---
+++ vm/vm.h
@@ -0,0 +1,31 @@
+#ifndef FUNVM_VM_H
+#define FUNVM_VM_H
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+	op_iconst,
+	op_add,
+	op_sub,
+	op_mul,
+	op_div,
+	op_negate,
+	op_ret,
+} OpCode;
+
+typedef struct {
+	uint32_t count;
+	uint32_t capacity;
+	uint8_t* code;
+	ValueArray constants;
+} Bytecode;
+
+uint32_t* lines;
+
+void initBytecode(Bytecode* bCode);
+void freeBytecode(Bytecode* bCode);
+void writeBytecode(Bytecode* bCode, uint8_t byte, uint32_t line);
+int addConstant(Bytecode* chunk, i32 value);
+
+#endif /* FUNVM_VM_H */
